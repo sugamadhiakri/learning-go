4f7f9efb4d81ee776dad46fc13e94ed027cf8291
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2022-08-02 01:30:05 +0545

    templating complete

---
+++ Learn Go with Tests/18 Templating/blogrenderer_test.go
@@ -0,0 +1,46 @@
+package blogrenderer
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Render(io.Discard, aPost)
+	}
+}
